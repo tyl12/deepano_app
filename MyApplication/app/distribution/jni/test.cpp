4f2ad157a1408324445af36ff5f6da6a34aa1845
Teng Yiliang <yiliang.teng@weimob.com>
2018-11-29 13:18:46 +0800

    start to port dp.
    Signed-off-by: Teng Yiliang <yiliang.teng@weimob.com>

---
+++ MyApplication/app/distribution/jni/test.cpp
@@ -0,0 +1,9 @@
+#include "test.h"
+#include <iostream>
+
+using namespace std;
+
+void A::test(){
+    cout<<__FUNCTION__<<endl;
+};
+
---
+++ MyApplication/app/distribution/obj/local/arm64-v8a/objs/test/test.o.d
@@ -0,0 +1,188 @@
+/home/tony/work/deep/DA/MyApplication/app/distribution/obj/local/arm64-v8a/objs/test/./test.o: \
+  /home/tony/work/deep/DA/MyApplication/app/distribution/jni/./test.cpp \
+  /home/tony/work/deep/DA/MyApplication/app/distribution/jni/test.h \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/iostream \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__config \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/ios \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/iosfwd \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/wchar.h \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__locale \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/string \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/string_view \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__string \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/algorithm \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/initializer_list \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstddef \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/type_traits \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstring \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/string.h \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/utility \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__tuple \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdint \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__debug \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/memory \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/typeinfo \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/exception \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdlib \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stdlib.h \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/new \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/limits \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/iterator \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__functional_base \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/tuple \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stdexcept \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cassert \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/atomic \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/functional \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdio \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stdio.h \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cwchar \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cwctype \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cctype \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/ctype.h \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/wctype.h \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/mutex \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__mutex_base \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/chrono \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/ctime \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/ratio \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/climits \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/system_error \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cerrno \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/errno.h \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__threading_support \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/locale.h \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/support/android/locale_bionic.h \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/support/xlocale/__posix_l_fallback.h \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/streambuf \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/istream \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/ostream \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/locale \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdarg \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__bsd_locale_fallbacks.h \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/bitset \
+  /home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__bit_reference
+
+/home/tony/work/deep/DA/MyApplication/app/distribution/jni/test.h:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/iostream:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__config:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/ios:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/iosfwd:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/wchar.h:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__locale:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/string:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/string_view:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__string:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/algorithm:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/initializer_list:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstddef:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/type_traits:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstring:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/string.h:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/utility:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__tuple:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdint:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__debug:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/memory:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/typeinfo:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/exception:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdlib:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stdlib.h:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/new:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/limits:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/iterator:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__functional_base:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/tuple:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stdexcept:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cassert:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/atomic:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/functional:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdio:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/stdio.h:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cwchar:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cwctype:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cctype:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/ctype.h:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/wctype.h:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/mutex:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__mutex_base:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/chrono:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/ctime:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/ratio:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/climits:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/system_error:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cerrno:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/errno.h:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__threading_support:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/locale.h:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/support/android/locale_bionic.h:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/support/xlocale/__posix_l_fallback.h:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/streambuf:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/istream:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/ostream:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/locale:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/cstdarg:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__bsd_locale_fallbacks.h:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/bitset:
+
+/home/tony/Android/Sdk/ndk-bundle/sources/cxx-stl/llvm-libc++/include/__bit_reference:
